25cbda99a5d472f837c2d7bcf2d6c0cc31979d54
ElvisNdunguMbuthia <maishamagicast@gmail.com>
2025-05-16 18:57:24 +0300

    created tests and passed both

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,16 @@
 // Your tests here
+
+import { isPalindrome } from '../utils';
+
+describe('isPalindrome', () => {
+    test('returns true for a palindrome string', () => {
+        expect(isPalindrome('racecar')).toBe(true);
+    });
+    
+    test('returns false for a non-palindrome string', () => {
+        expect(isPalindrome('hello')).toBe(false);
+    });
+    
+   
+}
+)
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,5 @@
 // Your code here
+export function isPalindrome(string){
+    const reversedString = string.split('').reverse().join('');
+    return string === reversedString;
+}
